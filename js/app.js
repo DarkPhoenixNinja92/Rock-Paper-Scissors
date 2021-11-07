3ed47b572ad112cfda7ba0990c48365c2c6a81c4
Shaun Pour <scpour@gmail.com>
2021-11-07 10:05:59 -0500

    1.10

--- js/app.js
+++ js/app.js
@@ -1,52 +1,9 @@
-const titleScreen = document.querySelector(".title-screen");
-const gameScreen = document.querySelector(".game-screen");
-const startBtn = document.querySelector(".start-game");
-let playerChoice = document.querySelector(".player-choice");
-let compChoice;
-
-function playerChoose() {
-    let choice = playerChoice.innerHTML;
-}
-
-function compChoose() {
-    let randNum = Math.floor(Math.random() * (9 - 0 + 1)) + 0;
-    if (randNum <= 3) {
-        compChoice = "Rock";
-    } else if (randNum > 3 && randNum < 7) {
-        compChoice = "Paper";
-    } else {
-        compChoice = "Scissors";
-    }
-    console.log(compChoice);
-}
-
-function choiceCompare() {
-    playerChoose();
-    compChoose()
-    let outcome;
-    if (choice === compChoice) {
-        outcome = "The Game is a draw";
-    } else if (choice === "Rock" && compChoice === "Paper") {
-        outcome = "The computer wins";
-    } else if (choice === "Rock" && compChoice === "Scissors") {
-        outcome = "You win";
-    } else if (choice === "Paper" && compChoice === "Scissors") {
-        outcome = "The computer wins";
-    } else if (choice === "Paper" && compChoice === "Rock") {
-        outcome = "You win";
-    } else if (choice === "Scissors" && compChoice === "Rock") {
-        outcome = "The computer wins";
-    } else if (choice === "Scissors" && compChoice === "Paper") {
-        outcome = "You win";
-    }
-    alert(outcome);
-    return outcome;
-}
-
-startBtn.addEventListener('click', function() {
-    titleScreen.style.display = "none";
-    gameScreen.style.display = "flex";
-});
-
-document.addEventListener('click', playerChoose);
-playerChoice.addEventListener('click', choiceCompare);
+const userScore = 0;
+const compScore = 0;
+const userScoreSpan = document.getElementById("user-score");
+const compScoreSpan = document.getElementById("comp-score");
+const scoreBoard = document.querySelector(".score-board");
+const resultOutput = document.querySelector(".result");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
